c5a518a27c73f15650a2bf36b0a3b3b319b7e9d6
WalterGutierrez27 <waltergutierrez@seti.com.co>
2024-07-12 14:10:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,8 @@ if selected_project == 'CP4D':
             )
             try:
                 cp4d.main(ruta)
-                ruta_salida = os.path.join(ruta, "salida", "Reporte_Insignias_CP4D.csv")
+                #ruta_salida = os.path.join(ruta, "salida", "Reporte_Insignias_CP4D.csv")
+                ruta_salida = os.path.join(ruta_base_descargas, f"Reporte_Insignias_{selected_project}.csv")
                 os.makedirs(os.path.dirname(ruta_salida), exist_ok=True)
     
                 if os.path.exists(ruta_salida):
