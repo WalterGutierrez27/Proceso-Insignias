92fe8b4509cb2818340d99df9b13264c7a475362
WalterGutierrez27 <waltergutierrez@seti.com.co>
2024-07-12 19:19:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -156,10 +156,9 @@ if selected_project == 'CP4D':
                     
                     #os.makedirs(os.path.dirname(ruta_salida), exist_ok=True)
 
-                    if not os.path.exists(ruta_salida):
-                        with open(ruta_salida, 'r', encoding='latin-1') as file:
-                            lines = file.readlines()
-                            
+                    #if  os.path.exists(ruta_salida):
+                    with open(ruta_salida, 'r', encoding='latin-1') as file:
+                        lines = file.readlines()                            
 
                         if len(lines) >= 2:
                             penultima_linea = lines[-1].strip()
