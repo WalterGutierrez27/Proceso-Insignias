e652c3898a9a653fe50626ba25e75c551ee31eb3
WalterGutierrez27 <waltergutierrez@seti.com.co>
2024-07-15 16:51:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -146,10 +146,10 @@ if selected_project == 'CP4D':
                     unsafe_allow_html=True
                 )
                 try:
-                    #cp4d.main(ruta)
-                    cp4d.main('C:/Users/waltergutierrez/Downloads/CP4D')
+                    cp4d.main(ruta)
+                    #cp4d.main('C:/Users/waltergutierrez/Downloads/CP4D')
                     #ruta_salida = os.path.join(ruta, "Reporte_Insignias_CP4D.csv")
-                    ruta_salida = os.path.join(ruta_base_descargas, "Downloads", f"Reporte_Insignias_{selected_project}.csv")
+                    #ruta_salida = os.path.join(ruta_base_descargas, "Downloads", f"Reporte_Insignias_{selected_project}.csv")
                     st.write(ruta_salida)
                     #ruta_salida = ruta+"/"+"Reporte_Insignias_CP4D.csv"
                     
