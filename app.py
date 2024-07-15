ca0fcce143cbba4df4ca81b9db909c1e2bbb65f0
WalterGutierrez27 <waltergutierrez@seti.com.co>
2024-07-15 13:58:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,8 @@ if selected_project == 'CP4D':
             try:
                 cp4d.main(ruta)
                 nombrearchivo = "Reporte_Insignias_CP4D.csv"
-                ruta_salida = os.path.normpath(ruta, nombrearchivo)
+                #ruta_salida = os.path.normpath(ruta, nombrearchivo)
+                ruta_salida = os.path.normpath(os.path.join(ruta, nombre_archivo))
                 
                 if os.path.exists(ruta_salida):
                     with open(ruta_salida, 'r', encoding='latin-1') as file:
