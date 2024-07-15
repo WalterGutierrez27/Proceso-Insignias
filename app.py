46bb15f6db22913bec72a061c6e6e8c7949d5a33
WalterGutierrez27 <waltergutierrez@seti.com.co>
2024-07-15 11:17:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -70,14 +70,14 @@ st.markdown(estilo_imagen, unsafe_allow_html=True)
 col1, col2 = st.columns([1, 2])
 
 # Imagen
-col1.image("LogoSetiAio.jpg", caption='', width=100)
+col1.image(os.path.join("LogoSetiAio.jpg"), caption='', width=100, use_column_width=True)
 
 # Título
 col2.markdown("<h1 class='titulo'>Proceso Insignias Estandares Desarrollo </h1>", unsafe_allow_html=True)
 
 # Barra lateral (sidebar)
 # Cargar la imagen en el sidebar y alinearla a la derecha
-st.sidebar.image("descargar.jfif", caption='Insignias', width=100)
+st.sidebar.image(os.path.join("descargar.jfif"), caption='Insignias', width=100, use_column_width=True)
 
 # Opciones de proyectos y selección en el sidebar
 st.sidebar.markdown("</div>", unsafe_allow_html=True)
@@ -135,58 +135,55 @@ if selected_project == 'CP4D':
 
     if st.button("Procesar archivo"):
         if ruta:
-            ruta = ruta.strip()  # Eliminar espacios en blanco
-            if os.path.exists(ruta) and os.path.isdir(ruta):
-                st.markdown(
-                    """
-                    <div style="background-color: #000000; color: white; padding: 10px; border-radius: 5px;">
-                        El reporte se generó satisfactoriamente, si deseas mayor detalle, ¡pulsa el botón descargar!
-                    </div>
-                    <br>
-                    """,
-                    unsafe_allow_html=True
-                )
-                try:
-                    cp4d.main(ruta)
-                    ruta_salida = os.path.join(ruta, "salida", "Reporte_Insignias_CP4D.csv")
-                    os.makedirs(os.path.dirname(ruta_salida), exist_ok=True)
-
-                    if os.path.exists(ruta_salida):
-                        with open(ruta_salida, 'r', encoding='latin-1') as file:
-                            lines = file.readlines()
-
-                        if len(lines) >= 2:
-                            penultima_linea = lines[-2].strip()
-                            st.markdown(
-                                f"""
-                                <div style="background-color: #000000; color: white; padding: 10px; border-radius: 5px;">
-                                    El porcentaje de cumplimiento del proyecto {selected_project} es: {penultima_linea}
-                                </div>
-                                <br>
-                                """,
-                                unsafe_allow_html=True
-                            )
-                        else:
-                            st.warning("El archivo no tiene suficientes líneas para mostrar la penúltima línea.")
-
-                        with open(ruta_salida, 'rb') as file:
-                            file_data = file.read()
-
-                        st.download_button(
-                            label="Descargar reporte",
-                            data=file_data,
-                            file_name="Reporte_Insignias_CP4D.csv",
-                            mime="text/csv"
+            st.markdown(
+                """
+                <div style="background-color: #000000; color: white; padding: 10px; border-radius: 5px;">
+                    El reporte se generó satisfactoriamente, si deseas mayor detalle, ¡pulsa el botón descargar!
+                </div>
+                <br>
+                """,
+               unsafe_allow_html=True
+            )
+            try:
+                cp4d.main(ruta)
+                ruta_salida = os.path.join(ruta, "Reporte_Insignias_CP4D.csv")
+                #ruta_salida = os.path.join(ruta_base_descargas, f"Reporte_Insignias_{selected_project}.csv")
+                os.makedirs(os.path.dirname(ruta_salida), exist_ok=True)
+    
+                if os.path.exists(ruta_salida):
+                    with open(ruta_salida, 'r', encoding='latin-1') as file:
+                        lines = file.readlines()
+    
+                    if len(lines) >= 2:
+                        penultima_linea = lines[-1].strip()
+                        st.markdown(
+                            f"""
+                            <div style="background-color: #000000; color: white; padding: 10px; border-radius: 5px;">
+                                El porcentaje de cumplimiento del proyecto {selected_project} es: {penultima_linea}
+                            </div>
+                            <br>
+                            """,
+                            unsafe_allow_html=True
                         )
                     else:
-                        st.error(f"No se encontró el archivo en la ruta especificada: {ruta_salida}")
-
-                except PermissionError:
-                    st.error("Permiso denegado para acceder a esta ruta.")
-                except Exception as e:
+                        st.warning("El archivo no tiene suficientes líneas para mostrar la penúltima línea.")
+    
+                    with open(ruta_salida, 'rb') as file:
+                        file_data = file.read()
+    
+                    st.download_button(
+                        label="Descargar reporte",
+                        data=file_data,
+                        file_name="Reporte_Insignias_CP4D.csv",
+                        mime="text/csv"
+                    )
+                else:
+                    st.error(f"No se encontró el archivo en la ruta especificada: {ruta_salida}")
+    
+            except PermissionError:
+                st.error("Permiso denegado para acceder a esta ruta.")
+            except Exception as e:
                     st.error(f"Error al listar la ruta: {e}")
-            else:
-                st.error("La ruta introducida no existe o no es un directorio.")
         else:
             st.warning("Por favor, introduce una ruta.")
 else:
@@ -278,7 +275,7 @@ else:
                         unsafe_allow_html=True
                     )
                 else:
-                    st.warning("El archivo no tiene suficientes líneas para mostrar la penúltima línea.")
+                    st.warning("El archivo no tiene resultados por mostrar")
 
                 with open(ruta_salida, 'rb') as file:
                     file_data = file.read()
